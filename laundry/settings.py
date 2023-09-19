71845ffac89829ced0115e1133582149e71542d2
bemarchant <bemarchant@proton.me>
2023-09-19 20:10:20 -0300

    basic login for website

--- laundry/settings.py
+++ laundry/settings.py
@@ -101,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTH_USER_MODEL = 'login.Neighbor'
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -117,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- login/models.py
+++ login/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 import random
 import string
 
@@ -8,17 +9,23 @@ class Apartment(models.Model):
 
     def __str__(self):
         return self.number
-
-class Neighbor(models.Model):
-    neighbor_id = models.CharField(max_length=8, unique=True, blank=True, null=True)
+    
+class Neighbor(AbstractUser):
+    neighbor_id = models.CharField(max_length=8, unique=True, primary_key=True)
+    name = models.CharField(max_length=100, default='Benja Marchant')
+    age = models.IntegerField(default=30)
+    gender = models.CharField(max_length=1, default="O", choices=[('M', 'Hombre'), ('F', 'Mujer'), ('O', 'Otro')])
+    apartment = models.CharField(max_length=100, default='802')
+    phone = models.CharField(max_length=14, default='(+56)9XXXXXXXX')
 
     def generate_neighbor_id(self):
-        while True:
-            neighbor_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-            if not Neighbor.objects.filter(neighbor_id=neighbor_id).exists():
-                self.neighbor_id = neighbor_id
-                self.save()
-                break
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+    def save(self, *args, **kwargs):
+        if not self.neighbor_id:
+            self.neighbor_id = self.generate_neighbor_id()
+
+        super().save(*args, **kwargs)
 
 class Neighborhood(models.Model):
     neighbors = models.ManyToManyField(Neighbor)
