155aeeda2fd6c677efb8413f08dd173310d7e3f4
bemarchant <bemarchant@proton.me>
2023-09-26 15:21:51 -0300

    added more information in booking window

--- booking/views.py
+++ booking/views.py
@@ -11,13 +11,13 @@ def booking_status(request):
 
 def booking(request):
     machine = Machine.objects.all()
-    context = {'machine': machine}
-    print(machine)
+    neighbor = request.user
+
+    context = {'machine': machine, 'neighbor' : neighbor}
+
     if request.method == 'POST':
         selected_date = request.POST.get('date')
         selected_slot = request.POST.get('slot')
-        print(selected_date)
-        print(selected_slot)
 
         if date.fromisoformat(selected_date) < date.today():
             return render(request, 'booking/booking.html', {'error_message': 'Selected date is in the past'})
@@ -25,8 +25,7 @@ def booking(request):
         # Check if the selected slot is available for booking
         machine = Machine.objects.first()
         available_slots = machine.get_available_slots(selected_date)
-        print(f'machine : {machine}')
-        print(f'available_slots : {available_slots}')
+
         if selected_slot not in available_slots:
             return render(request, 'booking/booking.html', {'error_message': 'Selected slot is not available'})
 
